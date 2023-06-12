487843f0a31ca361812900d422e4514412e19b2f
chaewons21 <136100411+chaewons21@users.noreply.github.com>
2023-06-12 20:53:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.signal as signal
 
-num = [100]
-den = [1, 5, 6]  # (s+2)(s+3) 확장
+t = np.linspace(0, 10, 1000)
+y = 100 * (np.heaviside(t, 1) - np.exp(-2*t) - np.exp(-3*t)) + 100
 
-H = signal.TransferFunction(num, den)
-
-w = np.logspace(-2, 2, 1000)
-
-w, mag, phase = signal.bode(H, w)
-
-fig, (ax1, ax2) = plt.subplots(2, 1)
-ax1.semilogx(w, mag) 
-ax1.set_xlabel('Frequency [rad/s]')
-ax1.set_ylabel('Gain [dB]')
-ax1.set_title('Bode Plot - Gain')
-
-ax2.semilogx(w, phase) 
-ax2.set_xlabel('Frequency [rad/s]')
-ax2.set_ylabel('Phase [degrees]')
-ax2.set_title('Bode Plot - Phase')
-
-plt.tight_layout()
+plt.plot(t, y)
+plt.xlabel('Time')
+plt.ylabel('y(t)')
+plt.title('Step Response')
+plt.grid(True)
 plt.show()
