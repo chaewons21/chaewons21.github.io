a395465bed48b3f4cbdb69bc5323a06deef1068a
chaewons21 <136100411+chaewons21@users.noreply.github.com>
2023-06-12 22:16:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,25 @@
-git add.
-git commit -m "이채원"
-git push
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+# 전달함수의 분자와 분모 계수
+num = [100]
+den = [1, 5, 6]  # (s+2)(s+3) 확장
+
+# 전달함수 생성
+sys = signal.TransferFunction(num, den)
+
+# 시간 범위 설정
+t = np.linspace(0, 10, 1000)
+
+# 시스템 응답 계산
+t, y = signal.step(sys, T=t)
+
+# 응답곡선 그리기
+plt.plot(t, y)
+plt.xlabel('Time')
+plt.ylabel('Output')
+plt.title('Step Response')
+plt.grid(True)
+plt.show()
 
